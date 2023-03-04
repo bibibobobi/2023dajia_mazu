587435f0d0f223b8ab259252a264c345015a1730
bibibobobi <smilingcat33@gmail.com>
2023-03-04 20:04:32 +0800

    feat(index): fetch data in getStaticProps()

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,70 +10,17 @@ import SectionNews from 'components/section-news';
 import { useInView } from 'react-intersection-observer';
 import { useState, useEffect } from 'react';
 import makePlaylist from '../utils/make-playlist';
+import axios from 'axios';
 
-const data = [
-  {
-    Name: 'Youtube',
-    order: '1',
-    Image: '-',
-    'Link / Content': 'MYPVQccHhAQ',
-    Note: '前端會將 YT 影片嵌入在活動影音區塊，上限 8 支。不滿 8 支時，有幾支就抓幾支，讓它播完之後會從頭輪播',
-  },
-  {
-    Name: 'Youtube',
-    order: '2',
-    Image: '-',
-    'Link / Content': 'Dxcc6ycZ73M',
-    Note: '',
-  },
-  {
-    Name: 'Youtube',
-    order: '3',
-    Image: '-',
-    'Link / Content': 'https://www.youtube.com/watch?v=A7n_HEH4Te8',
-    Note: '',
-  },
-  {
-    Name: 'Youtube',
-    order: '4',
-    Image: '-',
-    'Link / Content': '',
-    Note: '',
-  },
-  {
-    Name: 'Youtube',
-    order: '5',
-    Image: '-',
-    'Link / Content': '',
-    Note: '',
-  },
-  {
-    Name: 'Youtube',
-    order: '6',
-    Image: '-',
-    'Link / Content': '',
-    Note: '',
-  },
-  {
-    Name: 'Youtube',
-    order: '7',
-    Image: '-',
-    'Link / Content': '',
-    Note: '',
-  },
-  {
-    Name: 'Youtube',
-    order: '8',
-    Image: '-',
-    'Link / Content': '',
-    Note: '',
-  },
-];
+interface Data {
+  Youtube: {
+    id: string;
+    title: string;
+  }[];
+  // Add any other properties and their types here
+}
 
-const playlist = makePlaylist(data);
-console.log(playlist);
-
-export default function Home(): JSX.Element {
+export default function Home({ data }: { data: Data }): JSX.Element {
   const { ref: ref1, inView: inView1 } = useInView({ threshold: 0.4 });
   const { ref: ref2, inView: inView2 } = useInView({ threshold: 0.2 });
   const { ref: ref3, inView: inView3 } = useInView({ threshold: 0.2 });
@@ -95,6 +42,9 @@ export default function Home(): JSX.Element {
       setActiveElement('news');
     }
   }, [inView1, inView2, inView3, inView4]);
+
+  const playlist = makePlaylist(data.Youtube);
+  console.log(playlist);
 
   return (
     <>
@@ -118,4 +68,17 @@ export default function Home(): JSX.Element {
       <BottomNav activeElement={activeElement} />
     </>
   );
+}
+
+export async function getStaticProps() {
+  const apiEndpoint =
+    'https://storage.googleapis.com/static-mnews-tw-dev/json/matsu2023.json';
+  const response = await axios.get(apiEndpoint);
+  const data = response.data;
+  console.log(data.Youtube);
+  return {
+    props: {
+      data,
+    },
+  };
 }
