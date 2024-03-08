cf03c8d5969b01ecefc3eed4b9876d0a00d490ba
bibibobobi <smilingcat33@gmail.com>
2024-03-08 13:22:36 +0800

    chore(2024dajia): change to 2024 dev json

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,21 @@
-import { useInView } from 'react-intersection-observer';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import { makePlaylist, makeAdList, gaScrollEvent } from '../utils/utils';
-import Head from 'next/head';
-import Navigation from 'components/nav';
-import SideMenu from 'components/side-menu';
-import BottomNav from 'components/nav-mob';
-import Header from 'components/header';
-import SectionVideo from 'components/section-video';
-import SectionIntro from 'components/section-intro';
-import SectionTime from 'components/section-time';
-import SectionNews from 'components/section-news';
-import AdPc from 'components/ad-pc';
-import AdMob from 'components/ad-mob';
-import MainLogos from 'components/main-logos';
-import Footer from 'components/footer';
-import ScrollToTopButton from 'components/scroll-to-top-button';
+import axios from "axios";
+import AdMob from "components/ad-mob";
+import AdPc from "components/ad-pc";
+import Footer from "components/footer";
+import Header from "components/header";
+import MainLogos from "components/main-logos";
+import Navigation from "components/nav";
+import BottomNav from "components/nav-mob";
+import ScrollToTopButton from "components/scroll-to-top-button";
+import SectionIntro from "components/section-intro";
+import SectionNews from "components/section-news";
+import SectionTime from "components/section-time";
+import SectionVideo from "components/section-video";
+import SideMenu from "components/side-menu";
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import { gaScrollEvent, makeAdList, makePlaylist } from "../utils/utils";
 
 export default function Home(): JSX.Element {
   const [data, setData] = useState({
@@ -34,31 +34,31 @@ export default function Home(): JSX.Element {
   const { ref: adRef1, inView: ad1inView } = useInView({ threshold: 0.1 });
   const { ref: adRef2, inView: ad2inView } = useInView({ threshold: 0.1 });
 
-  const [activeElement, setActiveElement] = useState<string>('');
+  const [activeElement, setActiveElement] = useState<string>("");
 
   // Send gaScrollEvents
   const [ad1Sent, setAd1Sent] = useState(false);
   const [ad2Sent, setAd2Sent] = useState(false);
 
   if (ad1inView && !ad1Sent) {
-    gaScrollEvent('滑到廣告（一）');
+    gaScrollEvent("滑到廣告（一）");
     setAd1Sent(true);
   }
 
   if (ad2inView && !ad2Sent) {
-    gaScrollEvent('滑到廣告（二）');
+    gaScrollEvent("滑到廣告（二）");
     setAd2Sent(true);
   }
 
   useEffect(() => {
     axios
-      .get('https://statics.mnews.tw/json/matsu2023.json')
+      .get("https://statics-dev.mnews.tw/json/matsu2024.json")
       .then((response) => {
         const data = response.data;
         setData(data);
       })
       .catch((error) => {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       });
   }, []);
 
@@ -71,15 +71,15 @@ export default function Home(): JSX.Element {
 
   useEffect(() => {
     if (inView1) {
-      setActiveElement('video');
+      setActiveElement("video");
     } else if (inView2) {
-      setActiveElement('intro');
+      setActiveElement("intro");
     } else if (inView3) {
-      setActiveElement('time');
+      setActiveElement("time");
     } else if (inView4) {
-      setActiveElement('news');
+      setActiveElement("news");
     } else {
-      setActiveElement('');
+      setActiveElement("");
     }
   }, [inView1, inView2, inView3, inView4]);
 
@@ -96,15 +96,15 @@ export default function Home(): JSX.Element {
       <SectionIntro innerRef={ref2} intro={Introduction} />
 
       <div ref={adRef1} />
-      <AdPc adListPc={adListPc} label='廣告（一）' />
-      <AdMob adListMobile={adListMobile} label='廣告（一）' />
+      <AdPc adListPc={adListPc} label="廣告（一）" />
+      <AdMob adListMobile={adListMobile} label="廣告（一）" />
 
       <SectionTime innerRef={ref3} />
       <SectionNews innerRef={ref4} relatedPost={relatedPost} />
 
       <div ref={adRef2} />
-      <AdPc adListPc={adListPc} label='廣告（二）' />
-      <AdMob adListMobile={adListMobile} label='廣告（二）' />
+      <AdPc adListPc={adListPc} label="廣告（二）" />
+      <AdMob adListMobile={adListMobile} label="廣告（二）" />
 
       <MainLogos />
       <Footer logoList={logoList} />
